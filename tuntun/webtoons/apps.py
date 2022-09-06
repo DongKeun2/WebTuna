5edee68af0b25cf61872756073b33a5bc2f825d7
김민성 <alstjd5255@naver.com>
2022-09-06 10:49:43 +0900

    fix : 잘못 푸쉬한 파일 삭제

--- tuntun/webtoons/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class WebtoonsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'webtoons'
--- tuntun/webtoons/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'webtoons'
-
-urlpatterns = [
-    path('<platfrom>/<type>',views.webtoonsResponse)
-]
