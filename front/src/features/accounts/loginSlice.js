ed9a5345251899de4e82168eb29ba91674cd35bd
DK <ehdrms121@ajou.ac.kr>
2022-09-14 10:17:39 +0900

    feat: loginslice 추가

---
+++ front/src/features/accounts/loginSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const loginSlice = createSlice({
+  name: "login",
+  initialState: {},
+  reducers: {},
+});
+
+// Action creators are generated for each case reducer function
+export const {} = loginSlice.actions;
+
+export default loginSlice.reducer;
--- front/src/store.js
+++ front/src/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import loginReducer from "./features/accounts/loginSlice";
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    login: loginReducer,
+  },
 });
 
 export default store;
