3ea25b1bfbcb8c2de879645ba9af2d9d1a7e001a
DongKeun2 <ehdrms121@ajou.ac.kr>
2022-09-15 10:10:10 +0900

    feat: 사진 미리보기 & 미리보기 삭제 구현

--- front/src/pages/common/UploadPage.jsx
+++ front/src/pages/common/UploadPage.jsx
@@ -8,6 +8,13 @@ function UploadPage() {
     console.log(event.target.files[0]);
     console.log(fileImage);
   };
+
+  const deleteFileImage = () => {
+    URL.revokeObjectURL(fileImage);
+    console.log(fileImage);
+    setFileImage("");
+  };
+
   return (
     <div>
       <h1>사진 업로드 페이지</h1>
@@ -17,6 +24,22 @@ function UploadPage() {
         accept="image/*"
         onChange={saveFileImage}
       />
+      <div>
+        {fileImage && (
+          <img alt="sample" src={fileImage} style={{ margin: "auto" }} />
+        )}
+        <button
+          style={{
+            width: "50px",
+            height: "30px",
+            cursor: "pointer",
+          }}
+          onClick={() => deleteFileImage()}
+        >
+          {" "}
+          삭제{" "}
+        </button>
+      </div>
     </div>
   );
 }
