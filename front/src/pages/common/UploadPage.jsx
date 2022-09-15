d10aa304fd68b04f2cbbe042277438ad279808e0
DongKeun2 <ehdrms121@ajou.ac.kr>
2022-09-15 10:04:30 +0900

    feat: 사진업로드 구현

--- front/src/pages/common/UploadPage.jsx
+++ front/src/pages/common/UploadPage.jsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
+
 function UploadPage() {
+  const [fileImage, setFileImage] = useState("");
+
+  const saveFileImage = (event) => {
+    setFileImage(URL.createObjectURL(event.target.files[0]));
+    console.log(event.target.files[0]);
+    console.log(fileImage);
+  };
   return (
     <div>
       <h1>사진 업로드 페이지</h1>
+      <input
+        name="imggeUpload"
+        type="file"
+        accept="image/*"
+        onChange={saveFileImage}
+      />
     </div>
   );
 }
