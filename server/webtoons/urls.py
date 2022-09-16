649407fed29443e3e8288f758f8d3f44fa75dd4e
hongjoo <dlghdwn10@naver.com>
2022-09-16 18:58:47 +0900

    bug: 쓸모없는 api 주석처리

--- server/webtoons/urls.py
+++ server/webtoons/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'webtoons'
 
 urlpatterns = [
-    path('<platfrom>/<type>',views.webtoonsResponse),
+    # path('<platfrom>/<type>',views.webtoonsResponse),
     path('<int:webtoonId>/detail',views.webtoonDetail),
     path('list/<int:pageNum>',views.webtoonList),
     path('<search>/<keyword>/<int:pageNum>',views.searchWebtoon)
