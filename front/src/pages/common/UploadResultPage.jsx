d5fb06df1c9895a2f12a67daa621115833450dd4
dongkeun2 <ehdrms121@ajou.ac.kr>
2022-09-16 16:47:15 +0900

    feat: 그림체 추천 결과페이지 정보

--- front/src/pages/common/UploadResultPage.jsx
+++ front/src/pages/common/UploadResultPage.jsx
@@ -1,4 +1,8 @@
+import { useSelector } from 'react-redux'
+
 function UploadResultPage() {
+  const webtoonInfo = useSelector(state => state.upload.webtoonInfo)
+
   return (
     <div>
       <h1>그림체추천 결과 페이지</h1>
