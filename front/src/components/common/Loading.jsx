a2e317b3b61b7c49928cbbabaa3d4394152f4950
DK <ehdrms121@ajou.ac.kr>
2022-09-21 15:39:30 +0900

    feat: 로딩페이지 대충구현

--- front/src/components/common/Loading.jsx
+++ front/src/components/common/Loading.jsx
@@ -1,9 +1,87 @@
+import styled, { keyframes } from "styled-components";
+import loadingImg from "../../assets/test/cute.png";
+
 function Loading() {
   return (
-    <div>
-      <h1>로딩중입니다.</h1>
-    </div>
-  )
+    <LoadingBox>
+      <LoadingMsg>로딩중입니다</LoadingMsg>
+      <ImgBox>
+        <ProfileImg src={loadingImg} />
+      </ImgBox>
+    </LoadingBox>
+  );
 }
 
-export default Loading
+const LoadingBox = styled.div`
+  display: flex;
+  width: 100%;
+  height: 790px;
+`;
+
+const LoadingMsg = styled.p`
+  position: absolute;
+  top: 55%;
+  left: 40%;
+  font-size: 40px;
+  font-weight: bold;
+  @media screen and (max-width: 600px) {
+    font-size: 25px;
+  }
+`;
+
+const move = keyframes`
+	0%{
+    top: 45%;
+  } 
+  10% {
+    top: 44%;
+  }
+  20% {
+    top: 43%;
+  }
+  30%{
+    top: 42%;
+  }
+  40%{
+    top: 41%;
+  }
+  50%{
+    top: 40%
+  }
+  60%{
+    top: 41%;
+  }
+  70%{
+    top: 42%
+  }
+  80%{
+    top: 43%;
+  }
+  90%{
+    top: 44%;
+  }
+  100%{
+    top: 45%;
+  }
+`;
+
+const ImgBox = styled.div`
+  position: absolute;
+  margin-top: -15%;
+  margin-left: -150px;
+  top: 45%;
+  left: 50%;
+  width: 300px;
+  height: 300px;
+  border-radius: 70%;
+  overflow: hidden;
+  animation: ${move} 1.3s 0.1s infinite; ;
+`;
+
+const ProfileImg = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+
+export default Loading;
