f762e9e92b0c82d11d07e6d0d0ab42019286fade
DK <ehdrms121@ajou.ac.kr>
2022-09-22 20:48:57 +0900

    feat: 검색페이지 ui

--- front/src/pages/common/SearchPage.jsx
+++ front/src/pages/common/SearchPage.jsx
@@ -1,4 +1,6 @@
 import { useSelector } from "react-redux";
+import styled from "styled-components";
+import AllToonList from "../../components/toonlist/AllToonList";
 
 export default function SearchPage() {
   const toonList = useSelector((state) => state.search.toonList);
@@ -7,14 +9,47 @@ export default function SearchPage() {
     console.log(toonList);
   }
   return (
-    <div>
-      <h1>검색 페이지 ^^</h1>
-      <button onClick={checkFetch}>웹툰 들어왔니?</button>
+    <PageBox>
+      <HeaderBox>
+        <h1>검색 결과</h1>
+      </HeaderBox>
       {toonList.length ? (
-        <div>검색된 웹툰 목록~</div>
+        <ToonListBox>
+          <AllToonList toons={toonList} />
+        </ToonListBox>
       ) : (
-        <div>검색 결과 없어용</div>
+        <EmptyBox>
+          <h2>검색 결과 없어용</h2>
+        </EmptyBox>
       )}
-    </div>
+    </PageBox>
   );
 }
+
+const PageBox = styled.div`
+  width: 90%;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 0.5vw;
+  border: solid 2px;
+  border-radius: 0.8rem;
+  background-color: #fff5c3;
+`;
+
+const HeaderBox = styled.div`
+  display: flex;
+  align-items: center;
+  padding-left: 0.8vw;
+`;
+
+const ToonListBox = styled.div`
+  display: grid;
+  width: 100%;
+  margin-bottom: 70px;
+  grid-template-columns: repeat(5, minmax(0, 1fr));
+`;
+
+const EmptyBox = styled.div`
+  padding: 4vw;
+  height: 60vh;
+`;
