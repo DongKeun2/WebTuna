5ffee7454a2aed6c86ab11eeee4e557781ad422e
YoungJun <2753dudwns@naver.com>
2022-09-23 15:34:25 +0900

    feat: NavBar 반응형 UI

--- front/src/components/common/NavBar.jsx
+++ front/src/components/common/NavBar.jsx
@@ -8,11 +8,13 @@ import ToonToonRecommend from '../../assets/navbar/ToonToonRecommend.png'
 
 const Nav = styled.div`
   position: fixed;
+  left: 0;
   bottom: 0;
   width: 100%; //1920px
-  left: 0;
   background-color: #feec91;
-  height: 65px;
+  height: 4vw;
+  min-height: 60px;
+  padding-top: 0.5vh;
   border-top: solid 2px black;
   border-right: solid 2px black;
   border-left: solid 2px black;
@@ -23,6 +25,7 @@ const Nav = styled.div`
 const Items = styled.div`
   width: 100%;
   text-align: center;
+<<<<<<< HEAD
 `
 
 const LeftItem = styled.div`
@@ -38,10 +41,31 @@ const RightItem = styled.div`
   padding-left: 50px;
   padding-right: 155px;
 `
+=======
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const ItemGroup = styled.div`
+  width: 45%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const Item = styled.div`
+  @media screen and (min-width: 720px) {
+    width: 150px;
+  }
+  height: 100%;
+`;
+>>>>>>> eece598 (feat: NavBar 반응형 UI)
 
 const Toontoon = styled.div`
   position: absolute;
   left: 50%;
+<<<<<<< HEAD
   margin-left: -50px;
 <<<<<<< HEAD
 =======
@@ -49,13 +73,42 @@ const Toontoon = styled.div`
 >>>>>>> 596aad8 (detail 페이지 수정)
   box-shadow: 5px 5px;
   border-radius: 50px;
+=======
+  margin-left: -45px;
+  bottom: 1.3vh;
+  box-shadow: 4px 4px;
+  border-radius: 100%;
+  background-color: white;
+>>>>>>> eece598 (feat: NavBar 반응형 UI)
   overflow: hidden;
 `
+
+const ToonImg = styled.img`
+  width: 90px;
+  height: 90px;
+`;
+
+const IconImg = styled.img`
+  @media screen and (max-width: 720px) {
+    position: absolute;
+    top: 50%;
+    transform: translate(0, -50%);
+  }
+`;
+
+const IconText = styled.p`
+  margin: 0;
+  text-align: center;
+  @media screen and (max-width: 720px) {
+    display: none;
+  }
+`;
 
 function NavBar() {
   return (
     <Nav>
       <Items>
+<<<<<<< HEAD
         <LeftItem>
           <Link to="/" style={{ textDecoration: 'none' }}>
             <div>
@@ -93,6 +146,41 @@ function NavBar() {
             <div>ToonBTI</div>
           </Link>
         </RightItem>
+=======
+        <ItemGroup>
+          <Item>
+            <Link to="/" style={{ textDecoration: "none" }}>
+              <IconImg src={Home} alt="홈" />
+              <IconText>홈</IconText>
+            </Link>
+          </Item>
+          <Item>
+            <Link to="/webtoonList" style={{ textDecoration: "none" }}>
+              <IconImg src={All} alt="전체 웹툰" />
+              <IconText>전체 목록</IconText>
+            </Link>
+          </Item>
+        </ItemGroup>
+        <Toontoon>
+          <Link to="/toontoon" style={{ textDecoration: "none" }}>
+            <ToonImg src={ToonToonRecommend} alt="툰툰추천" />
+          </Link>
+        </Toontoon>
+        <ItemGroup>
+          <Item>
+            <Link to="/upload" style={{ textDecoration: "none" }}>
+              <IconImg src={PaintStyleRecommend} alt="그림체로 웹툰 검색" />
+              <IconText>그림체로 웹툰 검색</IconText>
+            </Link>
+          </Item>
+          <Item>
+            <Link to="/toonbti" style={{ textDecoration: "none" }}>
+              <IconImg src={ToonBTI} alt="툰비티아이" />
+              <IconText>ToonBTI</IconText>
+            </Link>
+          </Item>
+        </ItemGroup>
+>>>>>>> eece598 (feat: NavBar 반응형 UI)
       </Items>
     </Nav>
   )
