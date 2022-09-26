975472421bca3c4081e3fb01f7c116c39003854c
DK <ehdrms121@ajou.ac.kr>
2022-09-26 18:03:50 +0900

    feat: 그림체 30개 데이터 추가

--- front/src/assets/addInfo/addInfoItem.js
+++ front/src/assets/addInfo/addInfoItem.js
@@ -1,29 +1,154 @@
-import item1 from "./1.png";
-import item2 from "./2.jpg";
-import item3 from "./3.jpg";
-import item4 from "./4.jpg";
-import item5 from "./5.jpg";
+import item10 from "./10.jpg";
+import item43 from "./43.jpg";
+import item64 from "./64.jpg";
+import item88 from "./88.jpg";
+import item244 from "./244.jpg";
+import item245 from "./245.jpg";
+import item275 from "./275.jpg";
+import item277 from "./277.jpg";
+import item626 from "./626.jpg";
+import item636 from "./636.jpg";
+import item833 from "./833.jpg";
+import item1012 from "./1012.jpg";
+import item1100 from "./1100.jpg";
+import item1115 from "./1115.jpg";
+import item1143 from "./1143.jpg";
+import item1209 from "./1209.jpg";
+import item1261 from "./1261.jpg";
+import item1328 from "./1328.jpg";
+import item1833 from "./1833.png";
+import item1851 from "./1851.png";
+import item1918 from "./1918.png";
+import item1923 from "./1923.png";
+import item1929 from "./1929.png";
+import item2039 from "./2039.png";
+import item2076 from "./2076.png";
+import item2193 from "./2193.png";
+import item2194 from "./2194.png";
+import item2260 from "./2260.png";
+import item2370 from "./2370.png";
+import item2654 from "./2654.png";
 
 const addInfoItem = [
   {
-    id: 1,
-    img: item1,
+    id: 10,
+    img: item10,
   },
   {
-    id: 2,
-    img: item2,
+    id: 43,
+    img: item43,
   },
   {
-    id: 3,
-    img: item3,
+    id: 64,
+    img: item64,
   },
   {
-    id: 4,
-    img: item4,
+    id: 88,
+    img: item88,
   },
   {
-    id: 5,
-    img: item5,
+    id: 244,
+    img: item244,
+  },
+  {
+    id: 245,
+    img: item245,
+  },
+  {
+    id: 275,
+    img: item275,
+  },
+  {
+    id: 277,
+    img: item277,
+  },
+  {
+    id: 626,
+    img: item626,
+  },
+  {
+    id: 636,
+    img: item636,
+  },
+  {
+    id: 833,
+    img: item833,
+  },
+  {
+    id: 1012,
+    img: item1012,
+  },
+  {
+    id: 1100,
+    img: item1100,
+  },
+  {
+    id: 1115,
+    img: item1115,
+  },
+  {
+    id: 1143,
+    img: item1143,
+  },
+  {
+    id: 1209,
+    img: item1209,
+  },
+  {
+    id: 1261,
+    img: item1261,
+  },
+  {
+    id: 1328,
+    img: item1328,
+  },
+  {
+    id: 1833,
+    img: item1833,
+  },
+  {
+    id: 1851,
+    img: item1851,
+  },
+  {
+    id: 1918,
+    img: item1918,
+  },
+  {
+    id: 1923,
+    img: item1923,
+  },
+  {
+    id: 1929,
+    img: item1929,
+  },
+  {
+    id: 2039,
+    img: item2039,
+  },
+  {
+    id: 2076,
+    img: item2076,
+  },
+  {
+    id: 2193,
+    img: item2193,
+  },
+  {
+    id: 2194,
+    img: item2194,
+  },
+  {
+    id: 2260,
+    img: item2260,
+  },
+  {
+    id: 2370,
+    img: item2370,
+  },
+  {
+    id: 2654,
+    img: item2654,
   },
 ];
 
