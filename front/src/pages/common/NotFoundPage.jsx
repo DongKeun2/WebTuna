64f78062d7d6b486a872109e3bbd3c27dd327dd9
DK <ehdrms121@ajou.ac.kr>
2022-09-26 14:37:31 +0900

    feat: 404페이지 모달 추가

--- front/src/pages/common/NotFoundPage.jsx
+++ front/src/pages/common/NotFoundPage.jsx
@@ -1,4 +1,20 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Swal from "../../components/common/SweetAlert";
+
 function NotFoundPage() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    Swal.fire({
+      title: "잘못된 접근입니다!",
+      text: "메인페이지로 이동합니다.",
+      icon: "info",
+      confirmButtonColor: "#feec91",
+      confirmButtonText: "확인",
+    });
+    navigate("/");
+  }, [navigate]);
   return (
     <div>
       <h1>404 Not Found</h1>
