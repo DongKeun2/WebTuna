2d42796b0dbf13125cdf59336359e0582ac1e000
DK <ehdrms121@ajou.ac.kr>
2022-09-30 13:57:30 +0900

    feat: 커서 이미지 추가

--- front/src/GlobalStyle.jsx
+++ front/src/GlobalStyle.jsx
@@ -1,9 +1,13 @@
 import { createGlobalStyle } from "styled-components";
+import cursor from "./assets/cursor/cursor1.png";
 
 const GlobalStyle = createGlobalStyle`
   body {
     background-color: #D1E2FF;
     margin: 0;
+    :hover {
+      cursor:  url(${cursor}) 0 0, auto;
+    }
   }
 `;
 
