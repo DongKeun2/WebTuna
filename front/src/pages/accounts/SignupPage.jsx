0d8b98da3a8609bc2d2b536cf7285b86f2c5c679
DK <ehdrms121@ajou.ac.kr>
2022-10-04 13:38:49 +0900

    fix: 이메일에 .찍은것도 유효성검사 통과

--- front/src/pages/accounts/SignupPage.jsx
+++ front/src/pages/accounts/SignupPage.jsx
@@ -70,7 +70,7 @@ function SignupPage() {
   }
   function chkEmail(str) {
     const reg_email =
-      /^([0-9a-zA-Z_-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
+      /^([0-9a-zA-Z._-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
 
     if (!reg_email.test(str)) {
       return false;
